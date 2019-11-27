8a111945d971bd6d16e93ed82a3b476ef60858ed
Taylor Wilhite <taylorwilhite@gmail.com>
2019-11-27 11:22:23 -0800

    remove linter errors

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const App = () => {
   return(
-    <div class="app-container">
-      <h1 class="text-secondary mx-auto text-center text-4xl">Hello World!</h1>
+    <div className="app-container">
+      <h1 className="text-secondary mx-auto text-center text-4xl">Hello World!</h1>
     </div>
   )
 }
