68677fdd1d7594cfefddf452a332d262d520784e
Taylor Wilhite <taylorwilhite@gmail.com>
2019-11-27 11:13:42 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return(
+    <div class="app-container">
+      <h1 class="text-secondary mx-auto text-center text-4xl">Hello World!</h1>
+    </div>
+  )
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const { colors } = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          default: '#4FD1C5',
+        },
+        secondary: {
+          default: '#68D391',
+        },
+      }
+    }
+  }
+}
